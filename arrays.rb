59ba699c57125e0b1203f2eb66f9c3ac86f80ec0
Justin Lee <justinslee201100@yahoo.com>
2023-08-02 16:38:58 -0600

    Add 4 ruby methods with explination

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ ice_cream = ["Vanilla", "Chocolate", "Strawberry", "Rocky road"]
 scoops = [1,2,3,4]
 weight = [1.5, 2.5, 3.5, 4.5]
 yummy = [false, true, false, true]
+ice_cream.pop 
+#this will remove the last element "Rocky road" and return it 
+scoops.shift
+#this will remove the first element "1" and return it
+weight.unshift(0.1)
+#this will add the element "0.1" to the begining of the array
+yummy.push(true)
+#this will add the element "true" to the end of the array
